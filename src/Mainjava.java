1cc6cd728d5ec77505fe46a3cc833ed2b6e1b00d
Milton <milton@Miltons-iMac.local>
2016-02-27 12:36:03 -0600

    añade método
    Este es un método de prueba

--- src/Mainjava.java
+++ src/Mainjava.java
@@ -3,5 +3,7 @@ public class Mainjava {
 	public static void main (String []  args){
 		System.out.println("hello world");
 	}
-
+	public static void metodo (){
+		System.out.println("Soy un metodo");
+	}
 }
